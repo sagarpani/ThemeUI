36309c7bb2f989b1e3302a8bf180a39bd4cee1be
Sagar Pani <sagarpani22@gmail.com>
2026-04-01 11:04:49 +0530

    Resolve Navbar Issue

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import ThemeBtn from "./ThemeBtn";
 
 function Navbar(){
     return(
-        <div className="sticky top-0 z-50 shadow-md w-full flex justify-between items-center md:px-10 px-6 py-4">
+        <div className="sticky top-0 z-200 shadow-md w-full flex justify-between items-center md:px-10 px-6 py-4 bg-white dark:bg-gray-900">
           <h1 className="md:text-2xl text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
             ThemeUI
           </h1>
